fcb2c7bce085644eceda306cfeef31bc2ab23968
AnmarAmmir <124633421+AnmarAmmir@users.noreply.github.com>
2026-04-27 09:17:24 -0400

    Add PaginatedResult model for pagination support

---
+++ Backend/EmployeeManagementAPI/Models/DTOs/PaginatedResult.cs
@@ -0,0 +1,13 @@
+namespace EmployeeManagementAPI.Models.DTOs
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Data { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (TotalCount + PageSize - 1) / PageSize;
+        public bool HasNextPage => PageNumber < TotalPages;
+        public bool HasPreviousPage => PageNumber > 1;
+    }
+}
